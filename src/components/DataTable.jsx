ccae0a8a668e91f51714efb606fb64f827009be7
Zachary Sanders <zacharysanders@gmail.com>
2023-08-22 15:02:38 -0400

    production build

---
+++ src/components/DataTable.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+
+const DataTable = ({ data }) => {
+    // declare state variables (sortedData, sortOrder, sortColumn) w/ corresponding updating function
+    const [sortedData, setSortedData] = useState(data);
+    const [sortOrder, setSortOrder] = useState('asc');
+    const [sortColumn, setSortColumn] = useState('');
+
+    // handle the sorting logic when a column header is clicked
+    const handleSort = (columnName) => {
+        const isSameColumn = sortColumn === columnName;
+        const newSortOrder = isSameColumn ? (sortOrder === 'asc' ? 'desc' : 'asc') : 'asc';
+        
+        // updater functions
+        setSortOrder(newSortOrder);
+        setSortColumn(columnName);
+    
+        const sorted = [...sortedData].sort((a, b) => {
+            const aValue = a[columnName];
+            const bValue = b[columnName];
+            console.log('a'+ a);
+            console.log('b' + b);
+            
+            // Data column
+            if (columnName === 'dob') {
+                const aDate = new Date(aValue);
+                const bDate = new Date(bValue);
+                return (aDate - bDate) * (newSortOrder === 'asc' ? 1 : -1);
+            }
+            // String (all other) columns
+            if (typeof aValue === 'string') {
+                return aValue.localeCompare(bValue) * (newSortOrder === 'asc' ? 1 : -1);
+            }
+        
+            return (aValue - bValue) * (newSortOrder === 'asc' ? 1 : -1);
+        });
+    
+        setSortedData(sorted);
+    };
+
+    const HeaderCell = ({ label, onClick }) => {
+        return <th onClick={onClick}>{label}</th>;
+    };
+  
+    const DataRow = ({ data }) => {
+        return (
+            <tr>
+                <td>{data.name}</td>
+                <td>{data.favoriteFood}</td>
+                <td>{data.favoriteColor}</td>
+                <td>{data.numberOfPets}</td>
+                <td>{data.dob}</td>
+            </tr>
+        );
+    };
+
+  return (
+    <table className="data-table">
+      <thead>
+        <tr>
+          <HeaderCell label={"Name"} onClick={() => handleSort('name')} />
+          <HeaderCell label={"Favorite Food"} onClick={() => handleSort('favoriteFood')} />
+          <HeaderCell label={"Favorite Color"} onClick={() => handleSort('favoriteColor')} />
+          <HeaderCell label={"Number Of Pets"} onClick={() => handleSort('numberOfPets')} />
+          <HeaderCell label={"Date of Birth"} onClick={() => handleSort('dob')} />
+        </tr>
+      </thead>
+      <tbody>
+        {sortedData.map((row) => (
+          <DataRow key={row.name} data={row} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default DataTable;
