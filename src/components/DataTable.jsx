decc2d18e6e6ea156c513c3a056860d2da29308c
Zachary Sanders <zacharysanders@gmail.com>
2023-08-25 14:19:57 -0400

    Sets row sort state to asc on name for onload / adds stronger conditional checking for column clicks

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -1,33 +1,32 @@
 import React, { useState } from 'react';
 
 const DataTable = ({ data }) => {
-    // declare state variables (sortedData, sortOrder, sortColumn) w/ corresponding updating function
-    const [sortedData, setSortedData] = useState(data);
+    const setInitialNameSort = [...data].sort((a, b) => a.name.localeCompare(b.name));
+    // declare state variables 
+    const [sortedData, setSortedData] = useState(setInitialNameSort);
     const [sortOrder, setSortOrder] = useState('asc');
     const [sortColumn, setSortColumn] = useState('');
 
     // handle the sorting logic when a column header is clicked
     const handleSort = (columnName) => {
+        let newSortOrder;
         const isSameColumn = sortColumn === columnName;
+
         // determine sorting order
-        const newSortOrder = isSameColumn ? (sortOrder === 'asc' ? 'desc' : 'asc') : 'asc';
-        
-        // updater functions
-        setSortOrder(newSortOrder);
-        setSortColumn(columnName);
+       if (!isSameColumn ? newSortOrder = '' : newSortOrder = (sortOrder === 'asc' ? 'desc' : 'asc'));
   
         // sorting callback
-        const sorted = [...sortedData].sort((a, b) => {
+        const sorted = [...data].sort((a, b) => {
             const aValue = a[columnName];
             const bValue = b[columnName];
-            
-            // Date column
+
+            // Date column - covert into date objs
             if (columnName === 'dob') {
-                // convert into date objects
                 const aDate = new Date(aValue);
                 const bDate = new Date(bValue);
                 return (aDate - bDate) * (newSortOrder === 'asc' ? 1 : -1);
             }
+
             // String columns (sans date)
             if (typeof aValue === 'string') {
                 return aValue.localeCompare(bValue) * (newSortOrder === 'asc' ? 1 : -1);
@@ -35,7 +34,10 @@ const DataTable = ({ data }) => {
 
             return (aValue - bValue) * (newSortOrder === 'asc' ? 1 : -1);
         });
-    
+        
+        // updater functions
+        setSortOrder(newSortOrder);
+        setSortColumn(columnName);
         setSortedData(sorted);
     };
 
